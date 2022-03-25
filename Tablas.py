871c1f685303eaafed2af6093a3e7b73f5131075
Ivan Dominguez <i.dominguezr.2019@alumnos.urjc.es>
2022-03-25 12:29:32 +0100

    continuación tablas.py

--- Tablas.py
+++ Tablas.py
@@ -1,7 +1,18 @@
 import json
+import numpy as numpy
+import panda as panda
+
 
 with open('users.json') as file:
-    data = json.load(file)
+def parseUsersInfo():
 
+    data = json.load(file)
+    list_users = []
     for user in data['usuarios']:
-        print('cliente1' + user['sergio.garcia'])
+        nombre_user = list(user.keys())[0]
+        info_user = user[nombre_user]
+        del info_user["fechas"]
+        del info_user["ips"]
+        info_user['username'] = nombre_user
+        list_users.append(info_user)
+    return panda.json_normalize(list_users)
