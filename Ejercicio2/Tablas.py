5d446f7bb9b428b9677ff85d737f64f49d782651
Ivan Dominguez <i.dominguezr.2019@alumnos.urjc.es>
2022-03-27 19:08:29 +0200

    Creación de la BD

--- Ejercicio2/Tablas.py
+++ Ejercicio2/Tablas.py
@@ -1,46 +1,94 @@
-import json
-import pandas as panda
 import sqlite3
+import json
+
+conexion = sqlite3.connect('database.db')
+controlador = conexion.cursor()
 
 
-with open('users.json') as file:
-    data = json.load(file)
+def createDB():
+    controlador.execute("DROP TABLE IF EXISTS usuarios")
+    controlador.execute("DROP TABLE IF EXISTS fechas")
+    controlador.execute("DROP TABLE IF EXISTS ips")
+    controlador.execute("DROP TABLE IF EXISTS legal")
 
-    def parseUsersInfo():
-        list_users = []
-        for user in data['usuarios']:
-            nombre_user = list(user.keys())[0]
-            info_user = user[nombre_user]
-            del info_user["fechas"]
-            del info_user["ips"]
-            info_user['username'] = nombre_user
-            list_users.append(info_user)
+    controlador.execute("CREATE TABLE usuarios (id text, telefono integer, contrasena text, provincia text, "
+                      "permisos bool, emails_total integer, emails_phishing integer, emails_clicados integer, "
+                      "constraint PK_usuarios primary key (id)) ")
+    controlador.execute(
+        "CREATE TABLE fechas (id text, fecha text, constraint PK_fechas primary key (id,fecha), "
+        "constraint FK_fechas_usuarios foreign key (id) references usuarios(id))")
+    controlador.execute(
+        "CREATE TABLE ips (id text, ip text, constraint PK_ips primary key (id,ip), constraint "
+        "FK_ips_usuarios foreign key (id) references usuarios(id))")
 
 
-    def creacionBD():
-        conexion = sqlite3.connect("basededatos.db")
-        print("Base de datos abierta con exito")
-        conexion.execute('''CREATE TABLE usuarios
-                    (ID INT PRIMARY KEY NOT NULL,
-                    NOMBRE      TEXT    NOT NULL,
-                    TLFN        TEXT    NOT NULL,
-                    CONTRA      TEXT    NOT NULL,
-                    PROV        TEXT    NOT NULL,
-                    PERMISOS    TEXT    NOT NULL,
-                    FECHA       TEXT    NOT NULL,
-                    IPS         TEXT    NOT NULL);''')
+    controlador.execute("CREATE TABLE legal (url text, cookies integer, aviso integer, proteccion_de_datos integer,"
+                      "creacion integer, constraint PK_webs primary key (url))")
 
-        for info in data['usuarios']:
-            sql = "INSERT INTO usuarios(ID,NOMBRE,TLFN,CONTRA,PROV,PERMISOS,FECHA,IPS) VALUES('%s',%d,%d,%d,%d,%d,%d,%d)" % (
-            info['ID'], info['NOMBRE'], info['TLFN'], info['CONTRA'], info['PROV'], info['PERMISOS'],
-            info['FECHA'], info['IPS'])
+    conexion.commit()
 
-            conexion.execute(sql)
-            conexion.commit()
 
-        cursor = conexion.execute("select id,nombre,tlfn,contra,prov,permisos,fecha,ips from usuarios")
-        for row in cursor:
-            print('ID= ', row[0], 'Name= ', row[1], 'Telefono= ', row[2], 'Contraseña= ', row[3], 'Provincia= ', row[4], 'Permisos= ', row[5], 'Fecha= ', row[6], 'IPS= ', row[7])
+def rellenaTablasJson():
+    createDB()
 
-        print("operacion terminada")
-        conexion.close()
+    with open("users.json", "r") as file:
+        data = json.load(file)
+        for usuario in data["usuarios"]:
+            for id in usuario.keys():
+                query = "INSERT INTO usuarios (id) VALUES (\'{}\')".format(id)
+                controlador.execute(query)
+
+                if usuario[id]["telefono"] != 'None':
+                    query = "UPDATE usuarios SET telefono = {} WHERE id = \'{}\'".format(usuario[id]["telefono"], id)
+                    controlador.execute(query)
+
+                query = "UPDATE usuarios SET contrasena = \'{}\' WHERE id = \'{}\'".format(usuario[id]["contrasena"], id)
+                controlador.execute(query)
+
+                if usuario[id]["provincia"] != 'None':
+                    query = "UPDATE usuarios SET provincia = \'{}\' WHERE id = \'{}\'".format(usuario[id]["provincia"], id)
+                    controlador.execute(query)
+
+                query = "UPDATE usuarios SET permisos = {} WHERE id = \'{}\'".format(usuario[id]["permisos"], id)
+                controlador.execute(query)
+
+                query = "UPDATE usuarios SET emails_total = {} WHERE id = \'{}\'".format(usuario[id]["emails"]["total"], id)
+                controlador.execute(query)
+
+                query = "UPDATE usuarios SET emails_phishing = {} WHERE id = \'{}\'".format(usuario[id]["emails"]["phishing"],id)
+                controlador.execute(query)
+
+                query = "UPDATE usuarios SET emails_clicados = {} WHERE id = \'{}\'".format(usuario[id]["emails"]["cliclados"],id)
+                controlador.execute(query)
+
+                arrayFechas = []
+                for fecha in usuario[id]["fechas"]:
+                    if fecha not in arrayFechas:
+                        query = "INSERT INTO fechas (id,fecha) VALUES (\'{}\',\'{}\')".format(id, fecha)
+                        controlador.execute(query)
+                        arrayFechas.append(fecha)
+
+                arrayIps = []
+                for ip in usuario[id]["ips"]:
+                    if ip not in arrayIps:
+                        query = "INSERT INTO ips (id,ip) VALUES (\'{}\',\'{}\')".format(id, ip)
+                        controlador.execute(query)
+                        arrayIps.append(ip)
+    conexion.commit()
+
+    with open("legal.json", "r") as file:
+        data = json.load(file)
+
+        for legal in data["legal"]:
+            for url in legal.keys():
+                cookies = legal[url]['cookies']
+                aviso = legal[url]['aviso']
+                proteccion_de_datos = legal[url]['proteccion_de_datos']
+                creacion = legal[url]['creacion']
+                query = "INSERT INTO legal VALUES (\'{}\',\'{}\',\'{}\',\'{}\',\'{}\')".format(url, cookies, aviso,proteccion_de_datos, creacion)
+                controlador.execute(query)
+    conexion.commit()
+
+
+rellenaTablasJson()
+conexion.close()
