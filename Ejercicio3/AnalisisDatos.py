47f8cb9eb5ed5e8aff3c7b7580e1ab715006eec0
LiAN <o.mora.2019@alumnos.urjc.es>
2022-03-28 19:27:38 +0200

    Update AnalisisDatos.py EJ3

--- Ejercicio3/AnalisisDatos.py
+++ Ejercicio3/AnalisisDatos.py
@@ -6,46 +6,52 @@ import math
 conexion = sqlite3.connect('database.db')
 controlador = conexion.cursor()
 
+
 def numCorreos(rango: int):
     if rango < 0:
         rango = -rango
-        dataframe = pd.read_sql_query("SELECT * FROM usuarios WHERE emails_total >= \'{}\' group by id".format(rango), conexion)
+        dataframe = pd.read_sql_query("SELECT * FROM usuarios WHERE emails_total >= \'{}\' group by id".format(rango),
+                                      conexion)
 
-        dataframe["IPs"] = pd.read_sql_query("SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(username) WHERE emails_total "">= \'{}\' group by id".format(rango), conexion)
+        dataframe["IPs"] = pd.read_sql_query(
+            "SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(id) WHERE emails_total ""< \'{}\' group by id".format(
+                rango), conexion)
 
-        dataframe["Fechas"] = pd.read_sql_query("SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(username) WHERE ""emails_total >= \'{}\' group by id".format(rango), conexion)
+        dataframe["Fechas"] = pd.read_sql_query(
+            "SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(id) WHERE ""emails_total < \'{}\' group by id".format(
+                rango), conexion)
 
         return dataframe
     elif rango > 0:
 
-        dataframe = pd.read_sql_query("SELECT * FROM usuarios WHERE emails_total >= \'{}\' group by id".format(rango), conexion)
+        dataframe = pd.read_sql_query("SELECT * FROM usuarios WHERE emails_total >= \'{}\' group by id".format(rango),
+                                      conexion)
 
-        dataframe["IPs"] = pd.read_sql_query("SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(username) WHERE emails_total "">= \'{}\' group by id".format(rango), conexion)
+        dataframe["IPs"] = pd.read_sql_query(
+            "SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(id) WHERE emails_total "">= \'{}\' group by id".format(
+                rango), conexion)
 
-        dataframe["Fechas"] = pd.read_sql_query("SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(username) WHERE ""emails_total >= \'{}\' group by id".format(rango), conexion)
+        dataframe["Fechas"] = pd.read_sql_query(
+            "SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(id) WHERE ""emails_total >= \'{}\' group by id".format(
+                rango), conexion)
 
         return dataframe
     else:
         return "hay algun errror"
 
 
+def permisos(tipop: int):
+    if tipop == 1 or tipop == 0:
+        dataframe = pd.read_sql_query("SELECT * FROM usuarios WHERE permisos = \'{}\' group by id".format(tipop),
+                                      conexion)
 
+        dataframe["IPs"] = pd.read_sql_query(
+            "SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(id) WHERE permisos ""= \'{}\' group by id".format(
+                tipop), conexion)
 
-def permisos(permisos: int):
-    if permisos == 1:
-        dataframe = pd.read_sql_query("SELECT * FROM usuarios WHERE permisos = \'{}\' group by id".format(permisos), conexion)
-
-        dataframe["IPs"] = pd.read_sql_query("SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(username) WHERE permisos ""= \'{}\' group by id".format(permisos), conexion)
-
-        dataframe["Fechas"] = pd.read_sql_query("SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(username) WHERE ""permisos = \'{}\' group by id".format(permisos), conexion)
-
-        return dataframe
-    elif permisos == 0:
-        dataframe = pd.read_sql_query("SELECT * FROM usuarios WHERE permisos = \'{}\' group by id".format(permisos), conexion)
-
-        dataframe["IPs"] = pd.read_sql_query("SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(username) WHERE permisos ""= \'{}\' group by id".format(permisos), conexion)
-
-        dataframe["Fechas"] = pd.read_sql_query("SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(username) WHERE ""permisos = \'{}\' group by id".format(permisos), conexion)
+        dataframe["Fechas"] = pd.read_sql_query(
+            "SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(id) WHERE ""permisos = \'{}\' group by id".format(
+                tipop), conexion)
 
         return dataframe
     else:
@@ -55,6 +61,7 @@ def permisos(permisos: int):
 def observaciones(dataframe):
     return dataframe["emails_phising"].sum()
 
+
 def missing(dataframe):
     cont = 0
     for index, row in dataframe.iterrows():
@@ -62,20 +69,26 @@ def missing(dataframe):
             cont = cont + 1
     return cont
 
+
 def mediana(dataframe):
     return np.median(dataframe["emails_phising"].to_numpy())
+
 
 def media(dataframe) -> float:
     return dataframe["emails_phising"].sum / len(dataframe)
 
+
 def varianza(dataframe):
     return np.var(dataframe["emails_phishing"].to_numpy())
+
 
 def max(dataframe) -> int:
     return dataframe["emails_phishing"].max()
 
+
 def min(dataframe) -> int:
     return dataframe["emails_phishing"].min()
+
 
 dataframe_admins = permisos(1)
 dataframe_users = permisos(0)
@@ -96,7 +109,6 @@ print("Mediana Admins: ", mediana(dataframe_admins))
 print("Varianza Admins: ", varianza(dataframe_admins))
 print("Máximo Admins: ", max(dataframe_admins))
 print("Minimo Admins: ", max(dataframe_admins))
-
 
 print("#################### Para USERS ####################")
 print("Observaciones Users", observaciones(dataframe_users))
@@ -121,7 +133,6 @@ print("Varianza Admins: ", varianza(dataframe_masCorreo))
 print("Máximo Admins: ", max(dataframe_masCorreo))
 print("Minimo Admins: ", max(dataframe_masCorreo))
 
-
 print("#################### -200 correos ####################")
 print("Observaciones Users", observaciones(dataframe_menosCorreo))
 print("Valores ausentes en Users: ", missing(dataframe_menosCorreo))
@@ -130,26 +141,3 @@ print("Mediana Users: ", mediana(dataframe_menosCorreo))
 print("Varianza Users: ", varianza(dataframe_menosCorreo))
 print("Máximo Users: ", max(dataframe_menosCorreo))
 print("Minimo Users: ", max(dataframe_menosCorreo))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
